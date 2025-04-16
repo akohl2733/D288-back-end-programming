017e8617d810760effdf6b9644f72f63f8937bdd
Andrew Kohl <akohl2733@gmail.com>
2025-04-15 21:43:47 -0400

    Added Files from Spring Initializr to project

---
+++ src/test/java/ak288/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ak288.com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
